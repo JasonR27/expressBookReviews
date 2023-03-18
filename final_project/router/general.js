69c616adc66250483831021aca35c2006ca720d3
Jeison Roblero <jeisonroblero980@gmail.com>
2023-03-18 12:12:23 -0600

    Fifth Commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,18 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(300).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {  
@@ -19,44 +28,77 @@ public_users.get('/',function (req, res) {
   
   titles.push(Object.values(books).map(objVal => objVal.title + ' by ' + objVal.author)) 
 
+  
+
   return res.status(300).json({message: 'This are the Books available', titles});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookId = req.params.isbn;
+  console.log(bookId);
+
+  const book = books[bookId];
+
+  const isbnBook = [books[bookId].title + ' ' + 'by' + ' ' + books[bookId].author]
+  
+  return res.status(300).json({message: "Here is the book for your isbn: ", isbnBook: isbnBook});
  });
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  const author = req.params.author;
-  console.log(author);
-
-  const authorBook = [];
-
-  Object.values(books).forEach((book) => {
-    if (book.author === author) {
-      authorBook.push(book.title);
-    }
+    const author = req.params.author;
+    console.log(author);
+  
+    const authorBooks = [];
+  
+    Object.values(books).forEach((book) => {
+      if (book.author.toLowerCase() === author) {
+        authorBooks.push(book.title + ' ' + 'by' + ' ' + book.author);
+      }
+    });
+  
+    return res.status(300).json({
+      message: 'Here are the books from your author',
+      Books: authorBooks,
+    });
   });
-
-  return res.status(300).json({
-    message: 'Here are the books from your author',
-    authorBook: authorBook,
-  });
-});
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  console.log(author);
+  
+  const authorBooks = [];
+  
+  Object.values(books).forEach((book) => {
+      if (book.title.toLowerCase() === author) {
+        titleBooks.push(book.title + ' ' + 'by' + ' ' + book.author);
+      }
+    });
+  
+  return res.status(300).json({
+      message: 'Here are the books from your title',
+      Books: titleBooks,
+    });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookId = req.params.isbn;
+  console.log(bookId);
+
+  const review = books[bookId].review;
+
+  const isbnBook = [books[bookId].title]
+  
+  return res.status(300).json({
+      message: 'Here is the book for your isbn: ',
+      review: review,
+    });
 });
 
 module.exports.general = public_users;
